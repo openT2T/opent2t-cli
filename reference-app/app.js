3da311ca0d11f66049a2f45e4fef80372978cb5d
zhzoo <v-zhding@microsoft.com>
2018-02-15 08:49:01 -0800

    array use 1

--- reference-app/app.js
+++ reference-app/app.js
@@ -283,6 +283,9 @@ mainModule.controller('MainCtrl', ['$scope', '$http', '$q', 'remote', 'config', 
     }
     $scope.setTemperatureValue = function (device, property, value) {
         setDeviceProperty(device, property, { temperature: value, units: property.units }).then(info => {
+            if (Array.isArray(info) && info.length>0) {
+                info = info[0];
+            }
             property.temperature = info.temperature;
             if (!!info.units) {
                 property.units = info.units;
